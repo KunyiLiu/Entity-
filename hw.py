52ebb9ddee993291aa6fea6939c066e2b59ab455
KunyiLiu <kl2912@columbia.edu>
2017-03-25 01:13:21 +0800

    Create hw.py

---
+++ hw.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import re as re
+# read data
+amazon=pd.read_csv("amazon.csv")
+rotten_tomatoes=pd.read_csv("rotten_tomatoes.csv",encoding = "ISO-8859-1")
+train=pd.read_csv("train.csv")
+rotten_selected=rotten_tomatoes.iloc[:,:10]
+# deal with time
+prob=[]
+for index, row in rotten_selected.iterrows():  
+    timelong=row['time']
+    if str(timelong)!='nan':
+        m=re.search(r'(\d+) hr. (\d+) min.',timelong)
+        m1=re.search(r'(\d+) hr.',timelong)
+        m2=re.search(r'(\d+) min.',timelong)
+        if m:
+            time_tup=m.groups()
+            minute=int(time_tup[0])*60+int(time_tup[1])
+        elif m1:
+            time_tup=m1.groups()
+            minute=int(time_tup[0])*60
+        elif m2:
+            time_tup=m2.groups()
+            minute=int(time_tup[0])  
+        else:
+            prob.append(index)
+        rotten_selected.set_value(index, 'time', minute)
