ae5432ce530ff4817fe0a02ebac2194b00527675
KunyiLiu <kl2912@columbia.edu>
2017-03-25 13:55:57 +0800

    Update hw.py

--- hw.py
+++ hw.py
@@ -6,6 +6,42 @@ amazon=pd.read_csv("amazon.csv")
 rotten_tomatoes=pd.read_csv("rotten_tomatoes.csv",encoding = "ISO-8859-1")
 train=pd.read_csv("train.csv")
 rotten_selected=rotten_tomatoes.iloc[:,:10]
+## amazon data processing
+# deal with time period
+amazon = amazon.drop('cost', 1)
+for i in range(amazon.shape[0]):
+    starcol = str(amazon.loc[i]['star']) 
+    if bool(re.search(r'\d',starcol)):
+        amazon.set_value(i,'time',starcol)
+        amazon.set_value(i,'star',np.nan)
+    # cope with time column
+    timelong = str(amazon.loc[i]['time'])
+    if timelong.find('/') != -1:
+        amazon.set_value(i, 'time', np.nan)
+    else:        
+        h = int(timelong.find('hour'))
+        mins = int(timelong.find('minute'))
+        comma = int(timelong.find(','))
+        if h != -1:
+            hours = int(timelong[h-2])
+            minutes = int(timelong[comma+2:mins-1])
+        elif mins != -1:
+            hours = 0
+            minutes = int(timelong[:mins-1])
+        else:
+            amazon.set_value(i, 'time',np.nan)
+        amazon.set_value(i, 'time', hours*60 + minutes)
+# deal with star names
+for i in range(amazon.shape[0]):
+    # deal with star
+    starcol = str(amazon.loc[i]['star'])
+    if starcol.find('/')==-1:
+        starcol = set(starcol.split(','))
+    else:
+        starcol = set(starcol.split('/'))
+    amazon.set_value(i, 'star',starcol)
+    
+## rotten tomatoes data cleansing
 # deal with time
 prob=[]
 for index, row in rotten_selected.iterrows():  
